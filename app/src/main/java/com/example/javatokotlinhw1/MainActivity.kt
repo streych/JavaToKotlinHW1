2418625a2edec44179350ab3a526bbd4e093f25b
V radiulov <radiulovv.a@gmail.com>
2021-07-26 09:04:12 +1000

    migrate java to kotlin (MainActivity)

--- app/src/main/java/com/example/javatokotlinhw1/MainActivity.kt
+++ app/src/main/java/com/example/javatokotlinhw1/MainActivity.kt
@@ -1,14 +1,12 @@
-package com.example.javatokotlinhw1;
+package com.example.javatokotlinhw1
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
