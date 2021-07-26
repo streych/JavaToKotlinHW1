d69b708b427e291d6f802cf03a129ea0ced94d69
V radiulov <radiulovv.a@gmail.com>
2021-07-26 21:40:16 +1000

    Вывести значения из разных циклов в консоль, используя примеры из методических материалов.

--- app/src/main/java/com/example/javatokotlinhw1/MainActivity.kt
+++ app/src/main/java/com/example/javatokotlinhw1/MainActivity.kt
@@ -1,23 +1,48 @@
 package com.example.javatokotlinhw1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.button)
+        val textW = findViewById<TextView>(R.id.textW)
+        val textW2 = findViewById<TextView>(R.id.textW2)
+        val textW3 = findViewById<TextView>(R.id.textW3)
 
-        button.setOnClickListener { button.text = person2.title + " " + person2.description }
+        textW.text =  person2.title + " " + person2.description
+        button.setOnClickListener {
+            var sum  = 0
+            for (i in 1..10) {
+                sum += i
+                textW.text = sum.toString()
+            }
+            sum  = 0
+            for (i in 10 downTo 1 step 2){
+                sum += i
+                textW2.text = sum.toString()
+            }
+            sum  = 0
+            for (i in 0 until 5){
+                sum += i
+                textW3.text = sum.toString()
+            }
+        }
+
     }
 
     data class PersonClass(val title: String, val description: String)
 
     val person1 = PersonClass("Pipi", "Porklon")
     val person2 = person1.copy(title = "Pupi")
+
 
 }
